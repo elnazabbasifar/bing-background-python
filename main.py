edda361d51c170d6eb06cc6939d5e898578123b5
farxp <nicki_20002000@yahoo.com>
2020-12-06 04:07:52 +0330

    Refactored

--- main.py
+++ main.py
@@ -1,26 +1,33 @@
-from datetime import date
-import json
 import os
+import requests 
+from datetime import date
 
-import requests
-
-# $saveDir is used to set the location where Bing pics of the day are stored.
-save_dir="/home/eli/Pictures/.bing-images/"
-
-# get image url
+# get image url in jason format
+# idx parameter determines where to start from: 0 is the current day
 response = requests.get("https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US")
-image_data = json.loads(response.text)
+image_data = response.json()
 
+# Form the URL for the image
 image_url = image_data["images"][0]["url"].split("&")[0]
-full_image_url = "https://www.bing.com" + image_url
+full_url = "https://www.bing.com" + image_url
 
-# image's name: current date
+# get HOME path
+home_path = os.path.expanduser("~")
+
+# save_dir is used to set the location where Bing pics of the day are stored.
+save_dir = home_path + "/Pictures/.bing-images/"
+
+# create save directory if it does not already exist
+os.makedirs(save_dir, exist_ok=True)
+
+# image name: current date
 image_name = str(date.today())+".jpg"
 
 # download and save image
-response_full_url = requests.get(full_image_url)
-with open(save_dir+image_name, 'wb') as f:
-    f.write(response_full_url.content)
+res_full_url = requests.get(full_url)
+
+with open(save_dir + image_name, 'wb') as f:
+    f.write(res_full_url.content)
 
 # command to set wallpaper
-os.system("gsettings set org.gnome.desktop.background picture-uri file://"+save_dir+image_name)
+os.system("gsettings set org.gnome.desktop.background picture-uri file://" + save_dir+image_name)
